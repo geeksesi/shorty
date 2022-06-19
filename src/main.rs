4076383e433f688b06c9db6173fcb5f2563c1160
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-06-19 17:26:18 +0430

    refactor: cleanup main and move model to model :)

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,24 @@
 mod models;
 mod schema;
 
-use diesel::{Connection, PgConnection, RunQueryDsl};
-use dotenv::dotenv;
-use models::{NewUrl, Url};
-use schema::*;
-use std::env;
+use diesel::PgConnection;
+use models::Url;
+
+use crate::models::{create_url, establish_connection, get_url_by_key};
 
 #[macro_use]
 extern crate diesel;
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
-
-pub fn create_url(conn: &PgConnection, key: String, url: String) -> Url {
-    let new_url = NewUrl { key: key, url: url };
-
-    diesel::insert_into(urls::table)
-        .values(&new_url)
-        .get_result(conn)
-        .expect("Error saving new short url")
-}
-
 fn main() {
     let conn: PgConnection = establish_connection();
 
-    let url: Url = create_url(
-        &conn,
-        "geeksesi".to_string(),
-        "http://geeksesi.ir".to_string(),
-    );
+    // let url: Url = create_url(
+    //     &conn,
+    //     "geeksesi".to_string(),
+    //     "http://geeksesi.ir".to_string(),
+    // );
 
+    let key: String = "geeksesi".to_string();
+    let url: Url = get_url_by_key(&conn, &key);
     println!("{:?}", url);
 }
